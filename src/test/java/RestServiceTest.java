4ffa9a24bcfcdbdde68e8e1ea965f4d20d9b8765
Galkina <darja.galkin@gmail.com>
2025-05-09 21:30:08 +0300

    Add file csv

--- src/test/java/RestServiceTest.java
+++ src/test/java/RestServiceTest.java
@@ -1,19 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.RestService;
 
 public class RestServiceTest {
 
-    @Test
-    void shouldCalculateRestsOfYear() {
+    @ParameterizedTest
+    @CsvFileSource (files="src/test/resources/rest.csv")
+    public void shouldCalculateRestsOfYear(int expected, int income, int expense, int threshold) {
         RestService service = new RestService();
 
-        int income = 10000;
-        int expense = 3000;
-        int threshold = 20000;
-        int expected = 3;
-
-        long actual = service.calculate(income, expense, threshold);
+                int actual = service.calculate(income, expense, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
